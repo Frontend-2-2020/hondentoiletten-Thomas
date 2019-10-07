a0cdd2905fa4fb0c8836b1d39c915119ad565824
Thomas Velle <thomas.velle@gmail.com>
2019-10-07 20:37:27 +0200

    Update tijdens de pauze - map van Leaflet gegenereerd

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,21 @@
 import '../styles/index.scss';
 
-console.log('webpack starterkit');
+// Import leaflet API
+import L from "leaflet";
+
+// Import leaflet css
+import "leaflet/dist/leaflet.css";
+
+// initialize the map on the "map" div with a given center and zoom
+var map = L.map('map', {
+    center: [51.054633,3.7197544],
+    zoom: 18
+});
+
+// Add layer tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+// Add marker
+L.marker([51.054633,3.7197544]).addTo(map);
+
+
